c734a1e56789e526329ffa281fcd86ca3334cedf
sebastian-nasuta <32602175+sebastian-nasuta@users.noreply.github.com>
2022-12-06 00:10:54 +0100

    2 files modified

--- day-4/day-4.ts
+++ day-4/day-4.ts
@@ -8,10 +8,36 @@ export class Day4 implements DayEntity {
   }
 
   public resolve(): string {
-    return 'TODO';
+    const data = InputData.getData().split('\n');
+    let result = 0;
+    data.forEach((pair) => {
+      const pairRanges = pair.split(',');
+      const firstRange = pairRanges[0].split('-').map((x) => +x);
+      const secondRange = pairRanges[1].split('-').map((x) => +x);
+      if (
+        (firstRange[0] <= secondRange[0] && firstRange[1] >= secondRange[1]) ||
+        (secondRange[0] <= firstRange[0] && secondRange[1] >= firstRange[1])
+      ) {
+        result++;
+      }
+    });
+    return result.toString();
   }
 
   public resolve2(): string {
-    return 'TODO';
+    const data = InputData.getData().split('\n');
+    let result = 0;
+    data.forEach((pair) => {
+      const pairRanges = pair.split(',');
+      const firstRange = pairRanges[0].split('-').map((x) => +x);
+      const secondRange = pairRanges[1].split('-').map((x) => +x);
+      if (
+        (firstRange[0] <= secondRange[1] && firstRange[1] >= secondRange[0]) ||
+        (secondRange[0] <= firstRange[1] && secondRange[1] >= firstRange[0])
+      ) {
+        result++;
+      }
+    });
+    return result.toString();
   }
 }
