07ce2f69ae6b4076dcc7ea609c94b72944079d6a
sebastian-nasuta <32602175+sebastian-nasuta@users.noreply.github.com>
2022-12-05 23:15:43 +0100

    4 files modified

--- day-3/day-3.ts
+++ day-3/day-3.ts
@@ -8,10 +8,31 @@ export class Day3 implements DayEntity {
   }
 
   public resolve(): string {
-    return 'TODO';
+    const rucksacks = InputData.getData().split('\n');
+    let result = 0;
+    rucksacks.forEach((rucksack, i) => {
+      const firstCompartment = rucksack.slice(0, rucksack.length / 2);
+      const secondCompartment = rucksack.slice(
+        rucksack.length / 2,
+        rucksack.length
+      );
+      let done = false;
+      firstCompartment.split('').forEach((item) => {
+        if (!done && secondCompartment.includes(item)) {
+          result += this.getItemPriority(item);
+          done = true;
+        }
+      });
+    });
+    return result.toString();
   }
 
   public resolve2(): string {
     return 'TODO';
+  }
+
+  private getItemPriority(item: string): number {
+    const test = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return test.indexOf(item) + 1;
   }
 }
--- days.ts
+++ days.ts
@@ -1,4 +1,5 @@
 import { Day1 } from './day-1/day-1';
 import { Day2 } from './day-2/day-2';
+import { Day3 } from './day-3/day-3';
 
-export { Day1, Day2 };
+export { Day1, Day2, Day3 };
